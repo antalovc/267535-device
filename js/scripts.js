b2749581a0084498254b8557d9896029e4f7f43f
Anton <antal.ovchinnikov@gmail.com>
2017-02-17 16:10:26 +0300

    6.33 Исправлены карты, добавлена анимация неверного заполнения формы

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 var linkContact = document.querySelector(".index-contacts .btn");
 var popupContact = document.querySelector(".popup-contact");
 var closeContact = popupContact.querySelector(".popup-close");
+
+var contactForm = popupContact.querySelector("form");
 var contactName = popupContact.querySelector("[name=name]");
+var contactEmail = popupContact.querySelector("[name=email]");
+var contactText = popupContact.querySelector("[name=text]");
 
 var linkMap = document.querySelector(".index-contacts-map");
 var popupMap = document.querySelector(".popup-map");
@@ -16,6 +20,13 @@ linkContact.addEventListener("click", function (event) {
 closeContact.addEventListener("click", function (event) {
   event.preventDefault();
   popupContact.classList.remove("popup-show");
+  popupContact.classList.remove("popup-error");
+});
+
+contactForm.addEventListener("submit", function(event) {
+  if (!contactName.value || !contactEmail.value || !contactEmail.text) {
+    event.preventDefault();
+  }
 });
 
 linkMap.addEventListener("click", function (event) {
@@ -32,6 +43,7 @@ window.addEventListener("keydown", function (event) {
   if (event.keyCode === 27) {
     if (popupContact.classList.contains("popup-show")) {
       popupContact.classList.remove("popup-show");
+      popupContact.classList.remove("popup-error");
     }
     if (popupMap.classList.contains("popup-show")) {
       popupMap.classList.remove("popup-show");
